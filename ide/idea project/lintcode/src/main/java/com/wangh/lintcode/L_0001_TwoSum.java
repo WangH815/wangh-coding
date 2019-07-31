496361860e65f0b2a493c22f7b2aa43a086329b4
WangH <wangh_815@163.com>
2019-07-31 17:33:53 +0800

    update

--- ide/idea project/lintcode/src/main/java/com/wangh/lintcode/L_0001_TwoSum.java
+++ ide/idea project/lintcode/src/main/java/com/wangh/lintcode/L_0001_TwoSum.java
@@ -24,7 +24,7 @@ public class L_0001_TwoSum {
     }
 
     public int[] twoSum2(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
         }
@@ -38,7 +38,7 @@ public class L_0001_TwoSum {
     }
 
     public int[] twoSum3(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<Integer,Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int sub = target - nums[i];
             if (map.containsKey(sub)) {
