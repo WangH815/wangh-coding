5339695033c6490d1398242fbccee74b2b3bdf45
wangh <wangh_815@163.com>
2019-07-31 23:29:55 +0800

    update

---
+++ ide/idea project/lintcode/src/main/java/com/wangh/lintcode/L_0006_Convert.java
@@ -0,0 +1,54 @@
+package com.wangh.lintcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L_0006_Convert {
+    public static void main(String[] args) {
+        convert1("asdfasdf", 2);
+        convert2("asdfasdf", 2);
+    }
+
+    public static String convert1(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1)
+                goingDown = !goingDown; //遍历到两端，改变方向
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+    }
+
+    public static String convert2(String s, int numRows) {
+
+        if (numRows == 1)
+            return s;
+
+        StringBuilder ret = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) { //每次加一个周期
+                ret.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) //除去第 0 行和最后一行
+                    ret.append(s.charAt(j + cycleLen - i));
+            }
+        }
+        return ret.toString();
+    }
+
+}
