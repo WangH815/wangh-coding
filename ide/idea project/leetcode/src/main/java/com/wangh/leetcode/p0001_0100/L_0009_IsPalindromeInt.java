8be389f9aa4e658ec3bf3b9ce7e95f3de1c8c42d
wangh <wangh_815@163.com>
2019-08-11 00:22:03 +0800

    update

---
+++ ide/idea project/leetcode/src/main/java/com/wangh/leetcode/p0001_0100/L_0009_IsPalindromeInt.java
@@ -0,0 +1,56 @@
+package com.wangh.leetcode.p0001_0100;
+
+public class L_0009_IsPalindromeInt {
+    public static void main(String[] args) {
+        System.out.println(isPalindromeInt1(213));
+        System.out.println(isPalindromeInt2(213));
+        System.out.println(isPalindromeInt1(121));
+        System.out.println(isPalindromeInt2(121));
+    }
+
+    public static boolean isPalindromeInt1(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int rev = reverse(x);
+        return x == rev;
+    }
+
+    public static int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10)
+                return 0;
+            if (rev < Integer.MIN_VALUE / 10)
+                return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    public static boolean isPalindromeInt2(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int digit = (int) (Math.log(x) / Math.log(10) + 1); //总位数
+        int revert = 0;
+        int pop = 0;
+        //倒置右半部分
+        for (int i = 0; i < digit / 2; i++) {
+            pop = x % 10;
+            revert = revert * 10 + pop;
+            x /= 10;
+        }
+        if (digit % 2 == 0 && x == revert) {
+            return true;
+        }
+        //奇数情况 x 除以 10 去除 1 位
+        if (digit % 2 != 0 && x / 10 == revert) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ ide/idea project/leetcode/src/main/java/com/wangh/leetcode/p0001_0100/L_0028_IndexOfString.java
@@ -0,0 +1,45 @@
+package com.wangh.leetcode.p0001_0100;
+
+public class L_0028_IndexOfString {
+    public static void main(String[] args) {
+        String haystack = "hello,abcdef";
+        String needle = "abc";
+        System.out.println(indexOfString1(haystack, needle));
+        System.out.println(indexOfString2(haystack, needle));
+    }
+
+    public static int indexOfString1(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        int j = 0;
+        //遍历每个字符
+        for (int i = 0; i < haystack.length(); i++) {
+            //相等的话计数加 1
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+                //长度够了就返回
+                if (j == needle.length()) {
+                    return i - j + 1;
+                }
+                // 不相等的话 j 清零，
+                // 并且 i 回到最初的位置，之后就进入 for 循环中的 i++，从下个位置继续判断
+            } else {
+                i = i - j;
+                j = 0;
+            }
+        }
+        return -1;
+    }
+
+    public static int indexOfString2(String haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
+    }
+
+}
