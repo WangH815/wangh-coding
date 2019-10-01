3e709eafeb2a6e8216154ff70620ec83617c4fbb
wangh <wangh_815@163.com>
2019-10-02 01:12:09 +0800

    update

--- ide/idea project/leetcode/src/main/java/com/wangh/test/MyTest.java
+++ ide/idea project/leetcode/src/main/java/com/wangh/test/MyTest.java
@@ -1,7 +1,21 @@
 package com.wangh.test;
 
+import java.util.ArrayList;
+
 public class MyTest {
     public static void main(String[] args) {
-        System.out.println("afasdfawef");
+        String str = new String("afasdfawef");
+        String str1 = new String("afasdfawef");
+        String str2 = "afasdfawef";
+//        str.intern();
+        System.out.println(str.hashCode());
+        System.out.println(str1.hashCode());
+        System.out.println(str2.hashCode());
+
+        ArrayList arr = new ArrayList();
+        arr.add("0");
+        arr.add(0);
+        arr.remove(new Integer(0));
+
     }
 }
