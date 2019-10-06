d2d40eef7d42ff8aba3abde502a9f06358f0520b
wangh <wangh_815@163.com>
2019-10-07 01:18:04 +0800

    update

--- ide/idea project/leetcode/src/main/java/com/wangh/test/MyTest.java
+++ ide/idea project/leetcode/src/main/java/com/wangh/test/MyTest.java
@@ -3,7 +3,8 @@ package com.wangh.test;
 
 public class MyTest {
     public static void main(String[] args) {
-        int[] is = {6, 1, 7, 9, 0, 2};
+        int[] is = new int[]{6, 1, 7, 9, 0, 2};
+        int[][] iss = {{1,2,3},{1,2}};
         for (int i : is) {
             System.out.println(i);
         }
@@ -13,21 +14,31 @@ public class MyTest {
             System.out.println(i);
         }
 
+        System.out.println("aaa:" + (2+3)/2);
+
     }
 
-    private static int[] sortMaopao(int[] inta) {
+    private static int[] sortMaopao(int[] intArr) {
         int temp;
-        for (int i = 0; i < inta.length; i++) {
-            for (int j = 0; j < inta.length - i - 1; j++) {
-                if (inta[j] > inta[j + 1]) {
-                    temp = inta[j];
-                    inta[j] = inta[j + 1];
-                    inta[j + 1] = temp;
+        for (int i = 0; i < intArr.length; i++) {
+            for (int j = 0; j < intArr.length - i - 1; j++) {
+                if (intArr[j] > intArr[j + 1]) {
+                    temp = intArr[j];
+                    intArr[j] = intArr[j + 1];
+                    intArr[j + 1] = temp;
                 }
             }
         }
+        return intArr;
+    }
 
-        return inta;
+    private static int[] sortInsert(int[] intArr){
+        for(int i = 0;i <intArr.length;i++){
+            if(intArr[i]>intArr[i+1]){
+
+            }
+        }
+        return null;
     }
 
 }
