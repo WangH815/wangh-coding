edaa4d4e97608a937699f05e2eb6e5cc62470c69
wangh815 <wangh_815@163.com>
2020-05-20 04:09:38 +0800

    update

---
+++ ide/idea project/leetcode/src/main/java/com/wangh/t202005/T19.java
@@ -0,0 +1,56 @@
+package com.wangh.t202005;
+
+public class T19 {
+    T19(){
+        System.out.println("无参构造方法...");
+    }
+
+    public static void main(String[] args) {
+        T19 t19 = new T19();
+        myPrint("1234");
+
+        Student s =new Student();
+        System.out.println(s.getName());
+    }
+
+    // 先加载静态资源
+    static {
+        System.out.println("加载静态资源...");
+    }
+
+    public static void myPrint(final String context){
+        System.out.println(context);
+    }
+}
+
+
+class Student{
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private int id;
+    private String name;
+    private int age;
+
+}
