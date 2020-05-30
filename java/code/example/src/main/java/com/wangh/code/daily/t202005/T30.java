b7ebb13eaf84d49c67a176ea13cc029f954f0a7a
wangh815 <wangh_815@163.com>
2020-05-31 00:23:34 +0800

    update

---
+++ java/code/example/src/main/java/com/wangh/code/daily/t202005/T30.java
@@ -0,0 +1,10 @@
+package com.wangh.code.daily.t202005;
+
+public class T30 {
+    public static void main(String[] args) {
+        // Lambda表达式创建线程
+        new Thread(() -> {
+            System.out.println("new Thread!");
+        }).start();
+    }
+}
